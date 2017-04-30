7ac17471d3b6424ea2480945a32d6b7cd08a6a9d
Sean Hoar <sean.hoar@gmail.com>
2017-04-30 08:51:44 -0400

    Following along with the GithubProfile videos now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,43 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import t from 'prop-types';
 import Navbar from './Navbar.jsx';
 import Jumbotron from './Jumbotron.jsx';
 import Footer from './Footer.jsx'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: 'SHoar',
+      userData: [],
+      userRepos: [],
+      perPage: 5
+    }
+  }
   render(){
     return (
             <div>
               <Navbar />
-              <Jumbotron  />
-              <Footer />
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12">
+                    {this.props.clientId}
+                  </div>
+                </div>
+              </div>
             </div>
     );
   }
+}
+
+App.propTypes = {
+  clientId: t.string,
+  clientSecret: t.string
+}
+App.defaultProps = {
+  clientId: '40e087e1e45234da22c1',
+  clientSecret: '89a7d4d98ea6c646c9a45b41fbb69ff5b8c00441'
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,18 @@ export default class Navbar extends React.Component {
   render(){
     return (
         <div>
-          <nav className="nav navbar-default">
-            <ul className="nav navbar-default">
-              <li className="navbar-brand">Home</li>
-              <li className="navbar-brand">About</li>
-              <li className="navbar-brand">Contact</li>
-            </ul>
+          <nav className="navbar navbar-default">
+            <div className="container">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">My Github Profile</a>
+              </div>
+            </div>
           </nav>
         </div>
     );
